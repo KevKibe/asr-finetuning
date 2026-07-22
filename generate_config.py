5187d68c7e4e3af4c1aea42ca4890dc80b2ef274
KevKibe <kchegz234@gmail.com>
2026-07-22 23:59:50 +0300

    Adjust gradient accumulation steps to improve training performance in generate_config.py

--- generate_config.py
+++ generate_config.py
@@ -55,7 +55,7 @@ else:
         min_audio = 32_000
         max_audio = 320_000
         max_num_elements = 320_000
-        grad_accum = 2
+        grad_accum = 1
         use_fsdp = True
     else:  # CTC model
         num_steps = 10_000
